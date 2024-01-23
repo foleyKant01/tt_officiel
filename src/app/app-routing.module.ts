fa208d580ae6402549526b92e0aa17af7a764487
unknown <krayediego@gmail.com>
2024-01-23 15:33:24 +0100

    Login et Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)
+  },
+  {
+    path: 'super-admin',
+    loadChildren: () => import('./super-admin/super-admin.module').then((m) => m.SuperAdminModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule)
+  },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:AuthComponent,
+  children: [
+    { path:'', redirectTo:'login', pathMatch:'full' },
+    { path:'login', component:LoginComponent },
+    { path:'register', component:RegisterComponent },
+    { path:'forgotpassword', component:ForgotpasswordComponent },
+
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
