6611bab5c6a17908dbb6c093fd348589e8b0d34a
unknown <krayediego@gmail.com>
2024-01-23 17:11:49 +0100

    Login et Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+// import { HttpClientModule } from' @angular/common/http';
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdvertissementComponent } from './advertissement/advertissement.component';
-import { BusinessComponent } from './business/business.component';
-import { CategoriesComponent } from './categories/categories.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdvertissementComponent,
-    BusinessComponent,
-    CategoriesComponent,
-    NotfoundComponent
-  ],
+  declarations: [NotfoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
