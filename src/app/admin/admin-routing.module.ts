c3ae93d1346a643f4a387635cf20334c5955da30
foleyKant <krayediego@gmail.com>
2024-02-21 17:29:47 +0100

    mise a jour

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AdHomeComponent } from './ad-home/ad-home.component';
 import { CreateCategoriesComponent } from '../categories/create-categories/create-categories.component';
+import { ReadallCategoriesComponent } from '../categories/readall-categories/readall-categories.component';
+import { CreateBusinessComponent } from '../business/create-business/create-business.component';
+import { ReadallBusinessComponent } from '../business/readall-business/readall-business.component';
 
 const routes: Routes = [
   { path:'', component: AdminComponent,
@@ -10,6 +13,10 @@ const routes: Routes = [
     { path: '', redirectTo:'ad-home', pathMatch:'full' },
     { path:'ad-home', component: AdHomeComponent },
     { path:'create-categories', component: CreateCategoriesComponent },
+    { path:'readall-categories', component: ReadallCategoriesComponent },
+    { path:'create-business', component: CreateBusinessComponent },
+    { path:'readall-business', component: ReadallBusinessComponent },
+    {path: 'settings',loadChildren: () => import('./settings/settings.module').then((m) => m.SettingsModule)},
   ]
 }
 
--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesComponent } from './categories.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { ReadallCategoriesComponent } from './readall-categories/readall-categories.component';
 
 const routes: Routes = [
   { path:'', component:CategoriesComponent,
   children: [
     { path:'', redirectTo:'create-categories', pathMatch:'full' },
     { path:'create-categories', component:CategoriesComponent },
+    { path:'readall-categories', component:ReadallCategoriesComponent },
     { path:'spinner', component:SpinnerComponent },
     // { path:'register', component:RegisterComponent },
     // { path:'forgotpassword', component:ForgotpasswordComponent },
