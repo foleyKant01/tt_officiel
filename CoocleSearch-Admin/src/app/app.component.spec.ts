a0a99ce6f97a25e51a8dcc080b256b525f8ae3b3
foleyKant <krayediego@gmail.com>
2025-07-21 19:54:29 +0200

    ok

--- CoocleSearch-Admin/src/app/app.component.spec.ts
+++ CoocleSearch-Admin/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rfid_tracker_web' title`, () => {
+  it(`should have the 'admin_coocle' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rfid_tracker_web');
+    expect(app.title).toEqual('admin_coocle');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rfid_tracker_web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, admin_coocle');
   });
 });
