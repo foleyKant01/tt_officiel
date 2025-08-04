81faf31185986fb2132222d98b5b596641d5a928
foleyKant <krayediego@gmail.com>
2025-08-04 18:01:23 +0200

    ok

--- api/helpers/business.py
+++ api/helpers/business.py
@@ -432,7 +432,7 @@ def SearchBusinessByCategorie():
                     'bu_categorie': business.bu_categorie,
                     'bu_type': business.bu_type,
                     'bu_name': business.bu_name,
-                    'bu_description': business.bu_description,
+                    'description': item.description[:100] + "..." if len(item.description) > 50 else item.description
                     'bu_city': business.bu_city,
                     'bu_website': business.bu_website,
                     'phone': business.phone,
